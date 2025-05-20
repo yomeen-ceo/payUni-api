73fa46182cf4f228cda601f12e288c26639e7b1b
yomeen-admin <admin@yomeen.com>
2025-05-20 14:24:51 +0800

    1、架構改為MVC，原流程改成/order -> /v1/upp/create-order -> 付款 → /v1/upp/payment-return 2、merID、merKey、merIV改成本地使用dotenv管理，本地請建立.env檔儲存相關資訊 3、cloud run的env變數使用Google Cloud Secret Manager儲存密鑰，已預先儲存相關資訊，deploy到雲端的版本直接可以讀到merID、merKey、merIV。

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-const { encrypt, decrypt, sha256 } = require("./utils/payuni-crypto.js");
 const express = require('express')
 const bodyParser = require('express');
-const crypto = require('crypto');
-const qs = require('querystring');
 
 const v1RouteUpp =  require("./v1/routes/upp.js");
 
@@ -40,44 +37,6 @@ app.get('/order', (req, res) => {
       </body>
     </html>
   `);
-});
-
-/**
- * @api {post /result 接收payuni回傳前端資料，notify還沒寫
- * @apiName result
- * @apiParam {MerID}    EncryptInfo     商店代號
- * @apiParam {String}   Version         版本
- * @apiParam {String}   EncryptInfo     商品名稱
- * @apiParam {String}   HashInfo        商品金額
- *
- */
-app.post('/result', (req, res) => {
-    const raw = req.body;
-    console.log(JSON.stringify(raw));
-    // PayUni 會帶回這些欄位（含 EncryptInfo）
-    const { EncryptInfo } = raw;
-
-    if (!EncryptInfo) {
-        return res.send('<h3>錯誤：未收到交易資料</h3>');
-    }
-
-    // 解密
-    try {
-        const decrypted = decrypt(EncryptInfo, merKey, merIv);
-        const result = qs.parse(decrypted);
-        console.log(result)
-        // ✅ 顯示交易結果
-        res.send(`
-      <h2>交易結果</h2>
-      <p>狀態：${result.Status}</p>
-      <p>訂單編號：${result.MerTradeNo}</p>
-      <p>交易金額：${result.TradeAmt}</p>
-
-    `);
-    } catch (err) {
-        console.error('解密錯誤', err.message);
-        res.send('<h3>交易資料解析失敗</h3>');
-    }
 });
 
 // cloud run 要用8080port
