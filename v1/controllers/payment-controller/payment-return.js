27b9cf33b05c87f55496bb4b284b030e02bdaa3d
yomeen-admin <admin@yomeen.com>
2025-05-22 18:06:57 +0800

    新增幕後交易api，位於paymentController
    幕後交易api流程
    
    信用卡幕後交易流程：
    1、使用者前台下單，含信用卡資料
    2、前端送出訂單參數含信用卡資料
    3、後端呼叫幕後api，取得幕後3D驗證網址，回傳幕後3D驗證網址給前端
    4、前端讓使用者驗證，驗證完回到returnurl，同時notifyurl收到驗證成功或失敗的request
    
    備註：測試帳號直接驗證成功
    
    調整檔案與資料夾命名方式

---
+++ v1/controllers/payment-controller/payment-return.js
@@ -0,0 +1,36 @@
+const { decrypt } = require('../../../utils/payuni-crypto.js');
+const qs = require("querystring");
+module.exports = async (req, res) => {
+    const raw = req.body;
+    console.log(JSON.stringify(raw));
+    // PayUni 會帶回這些欄位（含 EncryptInfo）
+    const { EncryptInfo } = raw;
+
+    if (!EncryptInfo) {
+        return res.send('<h3>錯誤：未收到交易資料</h3>');
+    }
+
+    require('dotenv').config();
+    // 商店參數
+    const merKey = process.env.MER_KEY;
+    const merIv = process.env.MER_IV
+
+    // 解密
+    try {
+        const decrypted = decrypt(EncryptInfo, merKey, merIv);
+        const result = qs.parse(decrypted);
+        console.log(result)
+        // ✅ 顯示交易結果
+        res.send(`
+      <h2>這裡幕後交易完後自動回傳的網頁 /v1/payment/return</h2>
+      <h2>交易結果</h2>
+      <p>狀態：${result.Status}</p>
+      <p>訂單編號：${result.MerTradeNo}</p>
+      <p>交易金額：${result.TradeAmt}</p>
+
+    `);
+    } catch (err) {
+        console.error('解密錯誤', err.message);
+        res.send('<h3>交易資料解析失敗</h3>');
+    }
+}
